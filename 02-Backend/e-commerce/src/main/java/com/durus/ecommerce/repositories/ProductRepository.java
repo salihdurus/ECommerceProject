120d495b2c775f663c9fe58e9dcc8e02dd783d54
salihdurus <durussalih@gmail.com>
2023-08-18 10:11:03 +0300

    V1.0 is done

--- 02-Backend/e-commerce/src/main/java/com/durus/ecommerce/repositories/ProductRepository.java
+++ 02-Backend/e-commerce/src/main/java/com/durus/ecommerce/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.durus.ecommerce.repositories;
 
 import com.durus.ecommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
